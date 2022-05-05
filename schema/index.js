9ff6fbdfabacdf1a52290290723051f1037239be
koobawuah <me@bawuahboakye.com>
2022-05-05 11:13:17 +0000

    Import and export all schema models to application

---
+++ schema/index.js
@@ -0,0 +1,7 @@
+import User from "./User"
+import University from "./University"
+import Course from "./Course"
+import Review from "./Review"
+import Rating from "./Rating"
+
+export { User, University, Course, Review, Rating } 
