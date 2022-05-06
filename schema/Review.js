22e3eb43f6a19e35a2907ca14881f3b377387c79
koobawuah <me@bawuahboakye.com>
2022-05-06 15:32:14 +0000

    Added relationship field type to Review  model in list schema

--- schema/Review.js
+++ schema/Review.js
@@ -1,7 +1,8 @@
 import { list } from '@keystone-6/core'
-import { checkbox, text, timestamp, integer } from '@keystone-6/core/fields'
+import { checkbox, text, timestamp, integer, relationship } from '@keystone-6/core/fields'
 
 const Review = list({
+    ui: { labelField: 'reviewSummary' },
     fields: {
         reviewSummary: text({ ui: { displayMode: 'textarea' }}),
         pros: text(),
@@ -12,7 +13,22 @@ const Review = list({
         alumniYear: integer(),
         isAnon: checkbox(),
         inAppropriateCount: integer(),
-        //relationships
+        reviewBy: relationship({
+            ref: 'User.reviewsMade',
+            many: true,
+        }),
+        rating: relationship({
+            ref: 'Rating.reviewRating',
+            many: true,
+        }),
+        reviewsOfUni: relationship({
+            ref: 'University.reviews',
+            many: false,
+        }),
+        reviewsOfCourse: relationship({
+            ref: 'Course.reviews',
+            many: false,
+        }),
     }
 })
 
