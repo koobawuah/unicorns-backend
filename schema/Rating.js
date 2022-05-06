078ebf349ae9f8f5f6491d5539f06e9141b6e125
koobawuah <me@bawuahboakye.com>
2022-05-06 15:31:32 +0000

    Added relationship field type to Rating  model in list schema

--- schema/Rating.js
+++ schema/Rating.js
@@ -1,18 +1,31 @@
 import { list } from "@keystone-6/core";
-import { select, integer } from "@keystone-6/core/fields";
+import { select, float, relationship } from "@keystone-6/core/fields";
 
 const Rating = list({
+    ui: { labelField: 'rating' },
     fields: {
-        tile: select({ 
+        ratingType: select({ 
             defaultValue: 'Rating Options',
             options: [
+                { label: 'Overall Rating', value: 'OVERALL_RATING' },
                 { label: 'Facilities Rating', value: 'FACILITY_RATING' },
                 { label: 'Course Rating', value: 'COURSE_RATING' },
                 { label: 'Faculty Rating', value: 'FACULTY_RATING' },
                 { label: 'Recommendation', value: 'RECOMMEND_RATING' },
             ]}),
-        rating: integer(),
-        //relationships
+            rating: float(),
+            reviewRating: relationship({
+                ref: 'Review.rating',
+                many: false,
+            }),
+            ratingForUni: relationship({
+                ref: 'University.overallRating',
+                many: false,
+            }),
+            ratingForCourse: relationship({
+                ref: 'Course.courseRating',
+                many: false,
+            }),
     }
 })
 
