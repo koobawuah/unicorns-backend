e9e999f8acbc3a3a03250f1d5f49352765cbdce5
koobawuah <me@bawuahboakye.com>
2022-05-06 15:32:59 +0000

    Added relationship and image field types to University model in list schema

--- schema/University.js
+++ schema/University.js
@@ -1,15 +1,32 @@
 import { list } from "@keystone-6/core";
-import { text, integer, checkbox } from "@keystone-6/core/fields"
+import { text, integer, checkbox, relationship, image } from "@keystone-6/core/fields"
 
 const University = list({
     fields: {
         name: text({ validation: { isRequired: true } }),
         slug: text(),
+        campusImage: image(),
         description: text({ ui: { displayMode: "textarea" } }),
         location: text(),
+        country: text(),
         population: integer(),
         isApproved: checkbox(),
-
+        students: relationship({
+            ref: 'User.attendsUniversity',
+            many: true,
+        }),
+        hasCourses: relationship({
+            ref: 'Course.courseAt',
+            many: true,
+        }),
+        overallRating: relationship({
+            ref: 'Rating.ratingForUni',
+            many: true,
+        }),
+        reviews: relationship({
+            ref: 'Review.reviewsOfUni',
+            many: true,
+        }),
     }
 })
 
