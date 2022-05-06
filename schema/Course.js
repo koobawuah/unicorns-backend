1b0dedf49f7d7cd219be5f3d16dbb7730214f08e
koobawuah <me@bawuahboakye.com>
2022-05-06 15:31:15 +0000

    Added relationship field type to Course model in list schema

--- schema/Course.js
+++ schema/Course.js
@@ -1,15 +1,27 @@
 import { list } from "@keystone-6/core";
-import { text, checkbox } from "@keystone-6/core/fields";
+import { text, checkbox, relationship } from "@keystone-6/core/fields";
 
 
 const Course = list({
+    ui: { labelField: 'courseTitle' },
     fields: {
         courseCode: text({ isIndexed: 'unique', validation: {isRequired: true } }),
         courseTitle: text(),
         description: text({ ui: { displayMode: "textarea" } }),
         isPopular: checkbox(),
         isApproved: checkbox(),
-        //relationships
+        courseAt: relationship({
+            ref: 'University.hasCourses',
+            many: false,
+        }),
+        courseRating: relationship({
+            ref: 'Rating.ratingForCourse',
+            many: true,
+        }),
+        reviews: relationship({
+            ref: 'Review.reviewsOfCourse',
+            many: true,
+        }),
     }
 })
 
